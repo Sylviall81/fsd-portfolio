4f781696c1283c3487009559a7fece5375da8525
Sylvia Llorente <sylviall81@gmail.com>
2023-12-08 22:12:09 +0100

    adminpanel componente

--- front-end/src/components/AdminComponents/AddProjectForm.jsx
+++ front-end/src/components/AdminComponents/AddProjectForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
-export default  function AddProjectForm() {
+export default function AddProjectForm() {
   return (
     <form>
     <div className="space-y-12">
@@ -13,17 +13,17 @@ export default  function AddProjectForm() {
 
         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="sm:col-span-4">
-            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-              Username
+            <label htmlFor="project_title" className="block text-sm font-medium leading-6 text-gray-900">
+              Title
             </label>
             <div className="mt-2">
               <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                 <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
                 <input
                   type="text"
-                  name="username"
-                  id="username"
-                  autoComplete="username"
+                  name="project_title"
+                  id="project_title"
+                  autoComplete="Title"
                   className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   placeholder="janesmith"
                 />
--- front-end/src/components/Projects.jsx
+++ front-end/src/components/Projects.jsx
@@ -4,7 +4,10 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div>Projects
+    <div>
+      
+      <h1>
+      Projects/map/Cards </h1>
 
 {/* <ul className="divide-y divide-gray-200">
       {projects.map((project) => (
@@ -12,11 +15,72 @@ export default function Projects() {
           <img className="h-10 w-10 rounded-full" src={project.image} alt="" />
           <div className="ml-3">
             <p className="text-sm font-medium text-gray-900">{project.description}</p>
-            <p className="text-sm text-gray-500">{person.url_repository}</p>
+            <p className="text-sm text-gray-500">{project.url_repository}</p>
           </div>
         </li>
       ))}
     </ul> */}
+
+
+    {/*
+    OTRA POSIBLE VERSION 
+
+    Miniprojectos: Ver aqui para mostrar los prjectos en main
+    ver en: https://tailwindui.com/components/marketing/sections/team-sections
+
+const people = [
+  {
+    name: 'Leslie Alexander',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  // More people...
+]
+
+export default function Example() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper
+            suspendisse.
+          </p>
+        </div>
+        <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+          {people.map((person) => (
+            <li key={person.name}>
+              <div className="flex items-center gap-x-6">
+                <img className="h-16 w-16 rounded-full" src={person.imageUrl} alt="" />
+                <div>
+                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
+                  <p className="text-sm font-semibold leading-6 text-indigo-600">{person.role}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  */}
 
 
 
