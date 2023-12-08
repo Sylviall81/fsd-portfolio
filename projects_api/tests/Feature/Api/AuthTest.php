c37fc487305145f7697ee90164fb9cf352b18725
Sylvia Llorente <sylviall81@gmail.com>
2023-12-08 12:04:04 +0100

    user_can_register_test

--- projects_api/tests/Feature/Api/AuthTest.php
+++ projects_api/tests/Feature/Api/AuthTest.php
@@ -5,16 +5,29 @@ namespace Tests\Feature\Api;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class AuthTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic feature test example.
+     * Should create a new user.
      */
     public function test_example(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_user_can_register(): void
+    {
+        $this ->postJson('api/register',[
+            'name' => 'Patricia',
+            'email' => 'patricia2023@gmail.com',
+            'password' => Hash::make('123456789')
+        ]);
+
+        $this -> assertCount(1, User::all());
     }
 }
