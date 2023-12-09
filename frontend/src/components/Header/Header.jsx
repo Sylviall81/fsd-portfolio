f7f41f4d28308af5c931b384f825b578f197cd93
Sylvia Llorente <sylviall81@gmail.com>
2023-12-09 23:40:41 +0100

    cierre del dia

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="logo-container">
+          <img
+            src="https://res.cloudinary.com/dsesprxhl/image/upload/v1702051982/portfolio/logo_lc45fc.png"
+            alt=""
+          />
+        </div>
+
+      </header>
+    </>
+  );
+}
+
+export default Header;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Header from '../components/Header/Header'
 
 function Home() {
   return (
     <div>
         
-        <h1>
-        Home </h1>
+
+
+        <Header/>
+        <h1> Home </h1>
         
         </div>
   )
