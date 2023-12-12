f794f78f1bcbfbabe7fca283f54bed82f4ee1a5e
Sylvia Llorente <sylviall81@gmail.com>
2023-12-12 23:49:18 +0100

    borrar calling

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
 import Layout from "../components/Layout";
 
 export default function Portfolio() {
