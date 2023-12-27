5487c89237b24cc79085b0300976ea43e0657914
Sylvia Llorente <sylviall81@gmail.com>
2023-12-27 09:55:14 +0100

    Portfolio page structura inicial

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
+import NeutralButton from "../components/ButtonNeutral/NeutralButton";
+//import SectionImage from "../components/SectionImage/SectionImage";
 
 export default function Portfolio() {
   return (
     <div>
       <Layout>
-        
         <h1>Portfolio</h1>
+
+        <div>
+
+
+        {/* <SectionImage src={project.url} title ={project.title} text = {project.description} /> */}
+
+        <NeutralButton text="VIEW PROJECT" link="" />
+
+        </div>
+
       </Layout>
     </div>
   );
