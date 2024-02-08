571014cb21167d9b7d237f5c8c75bc864e59704f
Sylvia Llorente <sylviall81@gmail.com>
2024-02-08 15:39:10 +0100

    actualizacion

---
+++ frontend/src/components/MessagesTable/MessagesTable.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './MessgesTable.css'
+
+function MessagesTable() {
+  return (
+    <>
+
+    <table>
+
+    <tr>
+        <th></th>
+    </tr>
+
+
+
+    </table>
+
+
+
+
+    </>
+  )
+}
+
+export default MessagesTable
--- frontend/src/components/ProjectCarrousel/ProjectCarrousel.jsx
+++ frontend/src/components/ProjectCarrousel/ProjectCarrousel.jsx
@@ -7,32 +7,33 @@ import { useParams } from 'react-router-dom';
 function ProjectCarrousel({project, allProjects}) {
   const id = useParams
   const [currentId, setCurrentId] = useState(id);
-  //const [projectArray, setProjectArray] = useState(allProjects);
+  const [projects, setProjects] = useState(allProjects);
+  const [index, setIndex]= useState(0);
   console.log("currentID",currentId);
   console.log(allProjects.length);
 
 
-  const handleClickNext = ({currentId}) =>{
+  // const handleClickNext = ({currentId}) =>{
 
-    if (currentId > 0){
-      setCurrentId(currentId+1);
-      console.log(currentId);
-      return fetch(`/api/projects/${currentId}`)
+  //   if (currentId > 0){
+  //     setCurrentId(currentId+1);
+  //     console.log(currentId);
+  //     return fetch(`/api/projects/${currentId}`)
       
-    }
+  //   }
 
-  }
+  // }
 
-  const handleClickPrevious = ({currentId}) =>{
+  // const handleClickPrevious = ({currentId}) =>{
     
-    if (currentId <= allProjects.length){
-      setCurrentId(currentId-1);
-      console.log(currentId)
-      return fetch(`/api/projects/${currentId}`)
+  //   if (currentId <= allProjects.length){
+  //     setCurrentId(currentId-1);
+  //     console.log(currentId)
+  //     return fetch(`/api/projects/${currentId}`)
       
-    }
+  //   }
 
-  }
+  // }
     
 
   return (
@@ -40,7 +41,7 @@ function ProjectCarrousel({project, allProjects}) {
     <div className="project-slider-full-container">
 
     <div className="previous-project-container">
-            <button className="slider-invisible-button" onClick = {handleClickNext}>
+            <button className="slider-invisible-button" onClick = {() => setIndex (index-1)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16">
                 <path fill="none" stroke="#33323D" d="M9 0L1 8l8 8"/>
               </svg>
@@ -55,7 +56,7 @@ function ProjectCarrousel({project, allProjects}) {
 
 
     <div className="next-project-container">
-            <button className="slider-invisible-button" onClick={handleClickPrevious}>
+            <button className="slider-invisible-button" onClick={() => setIndex (index+1)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16">
                 <path fill="none" stroke="#33323D" d="M1 0l8 8-8 8"/>
               </svg>
