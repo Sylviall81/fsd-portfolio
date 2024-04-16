9ab6f8ec3eeab53443bbd178801beb0a0fe8ee3b
Sylvia Llorente <sylviall81@gmail.com>
2024-04-16 11:00:53 +0200

    tabla mensajes usuario

--- frontend/src/components/MessageBoard/MessageBoard.jsx
+++ frontend/src/components/MessageBoard/MessageBoard.jsx
@@ -11,21 +11,23 @@ function MessageBoard({contact_id,name,subject,message,phone,email, contact_date
     <table className="message-board-table">
       <thead>  
         <tr>
-          <th className="message-board-header-title" colSpan={6}> Recent Contacts </th>
+          <th className="message-board-header-title" colSpan="100%"> Recent Contacts </th>
         </tr> 
         <tr>
+          <th className="message-board-header-colspan">Id</th>
           <th className="message-board-header-colspan">Name</th>
           <th className="message-board-header-colspan" colSpan={2}>Contact Info</th>
+          <th className="message-board-header-colspan">Subject</th>{/*podria ser una especie de categoria con select*/} 
           <th className="message-board-header-colspan" colSpan={2}>Message</th>
           <th className="message-board-header-colspan">Date</th>
         </tr>
       </thead>
       <tbody>
         <tr>
+          <td className="message-board-cell">{contact_id}</td>
           <td className="message-board-cell">{name}</td>
+          <td className="message-board-cell"colSpan={2}> Phone:{phone} | Email:{email}</td>
           <td className="message-board-cell">{subject}</td>
-          <td className="message-board-cell">Phone:{phone}</td>
-          <td className="message-board-cell">Email:{email}</td>
           <td className="message-board-cell" colSpan={2}>{message}</td>
           <td className="message-board-cell">{contact_date}</td>
         </tr>
